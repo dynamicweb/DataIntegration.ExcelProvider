d62c8161cc132b1b54b83e8829e7e66b26fe33f4
Frederik Nielsen <frn@dynamicweb.dk>
2024-02-13 10:08:16 +0100

    Removing all relations to OleDB so we dont have a dependency towards Microsoft Access Database Engine and rely on EPPlus instead

--- src/ExcelReader.cs
+++ src/ExcelReader.cs
@@ -1,41 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using OfficeOpenXml;
+using System;
 using System.Data;
-using System.Data.OleDb;
 using System.IO;
 
 namespace Dynamicweb.DataIntegration.Providers.ExcelProvider
 {
-    public class ExcelReader
+    public class ExcelReader : IDisposable
     {
+        public string Filename { get; set; }
+        public DataSet ExcelSet { get; set; }
 
-        private String filename;
-        public virtual String Filename
-        {
-            get
-            {
-                return filename;
-            }
-            set
-            {
-                filename = value;
-            }
-        }
-
-        private DataSet excelSet;
-        public virtual DataSet ExcelSet
-        {
-            get
-            {
-                return excelSet;
-            }
-            set
-            {
-                excelSet = value;
-            }
-        }
-
-        public ExcelReader(String filename)
+        public ExcelReader(string filename)
         {
             Filename = filename;
             if (filename.EndsWith(".xlsx", StringComparison.OrdinalIgnoreCase) ||
@@ -46,84 +21,7 @@ namespace Dynamicweb.DataIntegration.Providers.ExcelProvider
                 {
                     if (File.Exists(filename))
                     {
-                        string strConn = ExcelProvider.GetOLEDB12ConnectionString(Filename);
-
-                        if (filename.EndsWith(".xls", StringComparison.OrdinalIgnoreCase))
-                        {
-                            strConn = ExcelProvider.GetOLEDB4ConnectionString(Filename);
-                        }
-
-                        DataSet ds = new DataSet();
-
-                        using (OleDbConnection conn = new OleDbConnection(strConn))
-                        {
-                            try
-                            {
-                                conn.Open();
-                            }
-                            catch (Exception)
-                            {
-                                throw;
-                            }
-
-                            DataTable schemaTable = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] { null, null, null, "TABLE" });
-
-                            foreach (DataRow schemaRow in schemaTable.Rows)
-                            {
-                                string sheet = schemaRow["TABLE_NAME"].ToString();
-
-                                if (!sheet.EndsWith("_") && sheet.Contains("$"))
-                                {
-                                    try
-                                    {
-                                        OleDbCommand cmd = new OleDbCommand("SELECT * FROM [" + sheet + "]", conn);
-                                        cmd.CommandType = CommandType.Text;
-
-                                        DataTable outputTable = new DataTable(sheet);
-                                        ds.Tables.Add(outputTable);
-                                        new OleDbDataAdapter(cmd).Fill(outputTable);
-                                        outputTable.Dispose();
-                                    }
-                                    catch (Exception ex)
-                                    {
-                                        throw new Exception(ex.Message + string.Format("Sheet: {0}.File.F{1}", sheet, Filename), ex);
-                                    }
-                                }
-                            }
-
-                            conn.Close();
-                            conn.Dispose();
-                        }
-
-                        ExcelSet = ds;
-
-                        foreach (DataTable dt in ExcelSet.Tables)
-                        {
-                            List<DataRow> deleteRows = new List<DataRow>();
-                            dt.TableName = dt.TableName.Replace("$", String.Empty);
-                            foreach (DataRow row in dt.Rows)
-                            {
-                                bool HasValue = false;
-                                foreach (DataColumn c in dt.Columns)
-                                {
-                                    string value = row[c].ToString();
-                                    if (!String.IsNullOrWhiteSpace(value) || !String.IsNullOrEmpty(value))
-                                    {
-                                        HasValue = true;
-                                        break;
-                                    }
-                                }
-                                if (HasValue == false)
-                                {
-                                    deleteRows.Add(row);
-                                }
-                            }
-                            foreach (DataRow row in deleteRows)
-                            {
-                                dt.Rows.Remove(row);
-                            }
-
-                        }
+                        LoadExcelFile();
                     }
                 }
             }
@@ -133,17 +31,38 @@ namespace Dynamicweb.DataIntegration.Providers.ExcelProvider
             }
         }
 
-        public static DataTable GetSchemaTable(string connectionstring)
+        private void LoadExcelFile()
         {
-            using (OleDbConnection connection = new OleDbConnection(connectionstring))
+            var fileInfo = new FileInfo(Filename);
+            using var package = new ExcelPackage(fileInfo);
+            var ds = new DataSet();
+            foreach (var worksheet in package.Workbook.Worksheets)
             {
-                connection.Open();
-                DataTable schemaTable = connection.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] { null, null, null, "TABLE" });
-                return schemaTable;
+                var dataTable = new DataTable(worksheet.Name);
+                bool hasHeader = true;
+                foreach (var firstRowCell in worksheet.Cells[1, 1, 1, worksheet.Dimension.End.Column])
+                {
+                    dataTable.Columns.Add(hasHeader ? firstRowCell.Text : string.Format("Column {0}", firstRowCell.Start.Column));
+                }
+
+                var startRow = hasHeader ? 2 : 1;
+                for (int rowNum = startRow; rowNum <= worksheet.Dimension.End.Row; rowNum++)
+                {
+                    var wsRow = worksheet.Cells[rowNum, 1, rowNum, worksheet.Dimension.End.Column];
+                    DataRow row = dataTable.Rows.Add();
+                    foreach (var cell in wsRow)
+                    {
+                        row[cell.Start.Column - 1] = cell.Text;
+                    }
+                }
+
+                ds.Tables.Add(dataTable);
             }
+
+            ExcelSet = ds;
         }
 
-        internal void Dispose()
+        public void Dispose()
         {
 
         }

02b46dce14e3aecefa2aae07b446f35f43685d6f
Dmitriy Benyuk <dbe@dynamicweb-cms.com>
2024-04-08 14:16:00 +0300

    Start reading data from the row where there is any data

--- src/ExcelReader.cs
+++ src/ExcelReader.cs
@@ -40,15 +40,18 @@ namespace Dynamicweb.DataIntegration.Providers.ExcelProvider
             var ds = new DataSet();
             foreach (var worksheet in package.Workbook.Worksheets)
             {
+                var firstRowNumberWithData = GetFirstRowNumberWithData(worksheet);
+                if (firstRowNumberWithData <= 0)
+                    continue;
+
                 var emptyRows = new List<DataRow>();
-                var dataTable = new DataTable(worksheet.Name);
-                var hasHeader = true;
-                var firstRow = worksheet.Cells[1, 1, 1, worksheet.Dimension.End.Column];
+                var dataTable = new DataTable(worksheet.Name);                
+                var firstRow = worksheet.Cells[firstRowNumberWithData, 1, firstRowNumberWithData, worksheet.Dimension.End.Column];
                 for (var colNum = 1; colNum <= worksheet.Dimension.End.Column; colNum++)
                 {
-                    var firstRowCell = firstRow[1, colNum];
+                    var firstRowCell = firstRow[firstRowNumberWithData, colNum];
                     DataColumn column;                    
-                    var header = hasHeader ? firstRowCell.Text : string.Format("Column {0}", firstRowCell.Start.Column);
+                    var header = firstRowCell.Text;
                     if (!dataTable.Columns.Contains(header) && !string.IsNullOrWhiteSpace(header))
                     {
                         column = dataTable.Columns.Add(header);
@@ -63,9 +66,8 @@ namespace Dynamicweb.DataIntegration.Providers.ExcelProvider
                         column.Caption = firstRowCell.Comment.Text;
                     }
                 }
-
-                var startRow = hasHeader ? 2 : 1;
-                for (var rowNum = startRow; rowNum <= worksheet.Dimension.End.Row; rowNum++)
+                
+                for (var rowNum = firstRowNumberWithData + 1; rowNum <= worksheet.Dimension.End.Row; rowNum++)
                 {
                     var hasValue = false;
                     var wsRow = worksheet.Cells[rowNum, 1, rowNum, worksheet.Dimension.End.Column];
@@ -90,6 +92,21 @@ namespace Dynamicweb.DataIntegration.Providers.ExcelProvider
             }
 
             ExcelSet = ds;
+        }
+
+        private static int GetFirstRowNumberWithData(ExcelWorksheet worksheet)
+        {
+            for (var rowNum = 1; rowNum <= worksheet.Dimension?.End?.Row; rowNum++)
+            {                
+                var wsRow = worksheet.Cells[rowNum, 1, rowNum, worksheet.Dimension.End.Column];                
+                for (var colNum = 1; colNum <= worksheet.Dimension.End.Column; colNum++)
+                {
+                    string cellText = wsRow[rowNum, colNum].Text;                    
+                    if (!string.IsNullOrWhiteSpace(cellText))
+                        return rowNum;
+                }
+            }
+            return -1;
         }
 
         public void Dispose()
